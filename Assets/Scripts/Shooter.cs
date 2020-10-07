fb698627f6e0545c83da09a84277126184b9630c
ara1098 <ara1098@gmail.com>
2020-10-06 20:28:10 -0400

    Read mouse to create halo and to instantiate projectile

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    [Header("Set in Inspector")]                                            
+    public GameObject prefabProjectile;
+
+    // fields set dynamically
+    [Header("Set Dynamically")]                                             
+    public GameObject launchPoint;
+    public Vector3 launchPos;                                  
+    public GameObject projectile;                               
+    public bool aimingMode;                                 
+
+   
+       // Start is called before the first frame update
+          void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");              
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+   
+
+    void OnMouseEnter()
+    {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+
+    }
+    void OnMouseExit()
+    {
+       // print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {                                                 
+        // The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        // Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        // Start it at the launchPoint
+        //projectile.transform.position = launchPos;
+        // Set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+
+}
