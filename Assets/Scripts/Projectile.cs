ff424be666a64db79f97ee1d9c3cba0fb9771bf5
ara1098 <ara1098@gmail.com>
2020-10-09 22:58:12 -0400

    GUI

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 public class Projectile : MonoBehaviour
 {
+
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+   
     public static float bottomY = -13.90522f;                                  // a
+    
+    void Start()
+    {
+        count = 0;
+        countText = GameObject.Find("CountText").GetComponent<TextMeshProUGUI>();
+        SetCountText();
+    }
     void Update()
     {
-
         
         if (transform.position.y == bottomY)
         {
@@ -19,6 +32,19 @@ public class Projectile : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.SetText("Count: " + count.ToString());
+
+        if (count >= 10)
+        {
+            winTextObject.SetActive(true);
         }
     }
 }
